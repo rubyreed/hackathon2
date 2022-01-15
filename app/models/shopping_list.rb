730f087da28ba2211577749abe87a0e2b648ad9c
Madeline Armstrong <armstrong.madeline@gmail.com>
2022-01-15 16:18:42 -0700

    sequel call for ingredients is done

--- app/models/shopping_list.rb
+++ app/models/shopping_list.rb
@@ -2,4 +2,13 @@ class ShoppingList < ApplicationRecord
   belongs_to :user
   has_many :ingredients
   has_many :recipes, through: :ingredients
+
+  # SELECT shopping_lists.id AS list_id, shopping_lists.user_id AS list_user_id, recipes.user_id AS recipe_user_id, recipes.id AS recipe_id, ingredients.name AS ingredient, ingredients.recipe_id AS ingredient_recipe from SHOPPING_LISTS
+  # INNER JOIN recipes ON shopping_lists.user_id = recipes.user_id
+  # INNER JOIN ingredients ON recipes.id = ingredients.recipe_id
+
+def self.ingredients(id)
+  select("shopping_lists.id AS list_id, shopping_lists.user_id AS list_user_id, recipes.user_id AS recipe_user_id, recipes.id AS recipe_id, ingredients.name AS ingredient, ingredients.recipe_id AS ingredient_recipe")
+  .joins("INNER JOIN users AS u ON showcases.user_id = u.id")
+  .joins("INNER JOIN ingredients ON recipes.id = ingredients.recipe_id")
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
 
-    resources :recipes
     resources :users, only: [:show, :update, :destroy]
     post "/users/image/:id", to: "users#image"
 
     get "/recipes", to: "recipes#index"
     get "/recipes/:id", to: "recipes#show"
     post "/recipes", to: "recipes#create"
-    delete "/recipes/:id", to: "recipes#delete
+    delete "/recipes/:id", to: "recipes#delete"
     resources :shopping_lists 
   end
 end
