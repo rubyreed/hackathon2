7ca07ee908bf6493a0c55b6710008206648faf0b
Sky Lin <linsky519@gmail.com>
2022-01-15 11:35:41 -0700

    merge request

--- client/src/pages/RecipeBook.js
+++ client/src/pages/RecipeBook.js
@@ -1,7 +1,26 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 const RecipeBook = () => {
+  const [recipes, setRecipes] = useState(null)
+
+  useEffect(() => {
+    getRecipeBooks()
+  }, [])
+
+  const getRecipeBooks = async () => {
+    try{
+      let res = await axios.get('/api/recipes')
+      setRecipes(res.data)
+    } catch(err){
+      alert('error in getRecipeBooks')
+    }
+  }
+
   return (
     <div>
       <h1>My Recipe Book</h1>
+      {JSON.stringify(recipes)}
     </div>
   )
 }
