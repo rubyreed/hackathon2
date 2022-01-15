2df382c95f5212af0855048b6d2d782fe0d47d3f
Ruby Reed <rubysreed@gmail.com>
2022-01-15 11:35:41 -0700

    navbar ready

--- client/src/providers/AuthProvider.js
+++ client/src/providers/AuthProvider.js
@@ -30,7 +30,7 @@ const AuthProvider = (props) => {
     try {
       let res = await axios.post("api/auth", user);
       setUser(res.data.data);
-      navigate("/protected");
+      navigate("/profile");
       // naviagate to a certain page
     } catch (err) {
       console.log(err.response);
@@ -44,7 +44,7 @@ const AuthProvider = (props) => {
       let res = await axios.post("api/auth/sign_in", user);
       setUser(res.data.data);
 
-      navigate("/protected");
+      navigate("/profile");
       // naviagate to a certain page
     } catch (err) {
       console.log(err.response);
